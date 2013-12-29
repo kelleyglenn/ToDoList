d0d87a86116a6b9828c0a075c49226c7097239d1
kelleyglenn <kelleyglenn+github@gmail.com>
2013-12-29 12:46:27 -0800

    Initial Commit

---
+++ ToDoList/KMGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KMGAppDelegate.h
+//  ToDoList
+//
+//  Created by Kelley Glenn on 12/29/13.
+//  Copyright (c) 2013 Kelley Glenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Kelley Glenn on 12/29/13.
+//  Copyright (c) 2013 Kelley Glenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KMGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KMGAppDelegate class]));
+    }
+}
